f43db6189fb9b2d71b5468da5f9790802e1ff505
Marissa Lea <ms.marissa.lea@gmail.com>
2018-06-26 21:52:22 -0600

    Menus

---
+++ AssetPipelineTest/Assets/Scripts/ActivateMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateMenu : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetMenu(GameObject inactiveMenu)
+    {
+        this.gameObject.SetActive(false);
+        inactiveMenu.SetActive(true);
+    }
+}
---
+++ AssetPipelineTest/Assets/Scripts/Mainmenuscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mainmenuscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetSelfAsInactive()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
